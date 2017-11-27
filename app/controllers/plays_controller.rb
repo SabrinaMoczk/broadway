87817a84201e4d30d64d131177a2a73272114a20
Sabrina Moczulski <sabrina@belasis.com.br>
2017-11-27 14:58:47 -0200

    Add Plays to App Index and Show Actions and View Pages

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,6 +1,11 @@
 class PlaysController < ApplicationController
 
 	def index
+		@plays = Play.all.order("created_at DESC")
+	end
+
+	def show
+		@play = Play.find(params[:id])
 	end
 
 	def new
@@ -9,6 +14,12 @@ class PlaysController < ApplicationController
 
 	def create
 		@play = Play.new(play_params)
+
+		if @play.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
 	end
 
 	def play_params
