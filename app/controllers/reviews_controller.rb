2f25f5b2e1e6a9a261c85ecb23a375cb0221f120
Sabrina Moczulski <sabrina@belasis.com.br>
2017-12-05 14:42:43 -0200

    Add View files for reviews and render on Play Show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,11 @@
 class ReviewsController < ApplicationController
+	before_action :find_play
 
 	def new
-		
 		@review = Review.new
 	end
 
 	def create
-		@play = Play.find(params[:play_id])
 		@review = Review.new(review_params)
 		@review.play_id = @play.id
 		@review.user_id = current_user.id
@@ -25,6 +24,6 @@ class ReviewsController < ApplicationController
 		end
 
 		def find_play
-			
+			@play = Play.find(params[:play_id])
 		end
 end
